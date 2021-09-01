8a5e03d07bce7039c7570d608a08844bfd91024f
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-09-01 14:53:48 -0400

    iniciales 2
    update

--- Projects/ASCII-initials.py
+++ Projects/ASCII-initials.py
@@ -6,4 +6,13 @@ M M M    I    FFF    FFF
 M   M    I    F      F
 M   M    I    F      F
 M   M  IIIII  F      F
+
+M     M  IIiIiII  FfFFFF  FfFFFF
+MM   MM    iIi    Ff      Ff
+MM m MM    iIi    Ff      Ff
+Mm M mM    iIi    FfFF    FfFF
+Mm   mM    iIi    Ff      Ff
+Mm   mM    iIi    Ff      Ff
+Mm   mM  IIiIiII  Ff      Ff 
+
 """)
