77a0742b280022db80743976d8ca8dbb2c2d5e39
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-09-01 14:36:41 -0400

    MIFF iniciales
    Usar command print para imprimir iniciales.

---
+++ Projects/ASCII-initials.py
@@ -0,0 +1,9 @@
+print("""
+M   M  IIIII  FFFFF  FFFFF
+MM MM    I    F      F 
+MM MM    I    F      F
+M M M    I    FFF    FFF
+M   M    I    F      F
+M   M    I    F      F
+M   M  IIIII  F      F
+""")
