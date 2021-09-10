50fcec00d3a40bac69fa2d4604baf5e10a68d64e
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-09-10 14:38:48 -0400

    Module 4 Meal
    Screenshot y code

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,11 @@
+print("What did you have for Breakfast?")
+meal = "Eggs and Bacon."
+print("meal")
+print("What was your Lunch?")
+meal = "Tacos with hot sauce."
+print("meal")
+print("Have you had dinner?")
+meal = ("I had some Lasagna")
+print("meal")
+
+
