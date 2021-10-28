48c25fc525ebc9bb1580b8514254d0dc4f4e6e1b
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-10-28 14:34:38 -0400

    motion p martin

--- Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
+++ Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
@@ -1,16 +1,19 @@
-weapon = 'VSS' 
-cartridge = '9x39mm'
-ammo = 'Full-Auto'
-velocity = 900
-
-building = 'Marina Torch'
-Marina = 352
-
-gravity = 9.81
-
 import math
-time = math.sqrt((2 * Marina) / gravity)
-distance = velocity * time
 
-print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires {ammo} rounds at {velocity} RPM. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
+# weapon = 'VSS' 
+# cartridge = '9x39mm'
+# ammo = 'SPP gs'
+# velocity = 310
 
+# building = 'Marina Torch'
+# Marina = 352
+
+# gravity = 9.81
+
+def ProjectileFunction(weapon:str, cartridge:str, ammo:str, velocity:int, building:str, Marina:int, gravity:int):
+    time = math.sqrt((2 * Marina) / gravity)
+    distance = (velocity * time)
+
+    print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires single {ammo} rounds at {velocity} meters per second. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
+
+ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
