9587bece5cbe7ce7d38283855358a301311347fc
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-11-03 14:49:10 -0400

    projectmotion update

--- Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
+++ Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
@@ -14,19 +14,23 @@ from ExperimentalData import ExperimentalData
 def ProjectileFunction(experimentalData:ExperimentalData):
     time = math.sqrt((2 * Marina) / gravity)
     # distance = (experimentalData[velocity] * time)
+    distance = (velocity * time)
 
-#     print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires single {ammo} rounds at {velocity} meters per second. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
+    print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires single {ammo} rounds at {velocity} meters per second. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
 
-# ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
+# Convert parameters (variables) into a JSON 
 
-(7 > 6) and (3 == 5) 
-not (7 > 6)
-(7 > 6) or (3 == 5)
+# experimentalData = {
+
+# 'weapon' : 'VSS', 
+# 'cartridge' : '9x39mm',
+# 'ammo' : 'SPP gs',
 # 'velocity' : 310,
 # 'building' : 'Marina Torch',
 # 'Marina' : 352,
 # 'gravity' : 9.81
 
 # }
+
 experimentalData = ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
 ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
