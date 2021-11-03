cdfada84eaf577168866c1ae340065f077f9f301
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-11-03 14:48:40 -0400

    Json object
    project motion

--- Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
+++ Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
@@ -1,4 +1,5 @@
-# import math
+import math
+from ExperimentalData import ExperimentalData
 
 # # weapon = 'VSS' 
 # # cartridge = '9x39mm'
@@ -10,9 +11,9 @@
 
 # # gravity = 9.81
 
-# def ProjectileFunction(weapon:str, cartridge:str, ammo:str, velocity:int, building:str, Marina:int, gravity:int):
-#     time = math.sqrt((2 * Marina) / gravity)
-#     distance = (velocity * time)
+def ProjectileFunction(experimentalData:ExperimentalData):
+    time = math.sqrt((2 * Marina) / gravity)
+    # distance = (experimentalData[velocity] * time)
 
 #     print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires single {ammo} rounds at {velocity} meters per second. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
 
@@ -21,3 +22,11 @@
 (7 > 6) and (3 == 5) 
 not (7 > 6)
 (7 > 6) or (3 == 5)
+# 'velocity' : 310,
+# 'building' : 'Marina Torch',
+# 'Marina' : 352,
+# 'gravity' : 9.81
+
+# }
+experimentalData = ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
+ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
