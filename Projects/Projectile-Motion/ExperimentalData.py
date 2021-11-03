b8dbcd7a0504983d19c801a380bc0f6fa86f0085
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-11-03 14:49:37 -0400

    ExperimentalData

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,10 @@
+class ExperimentalData: 
+    def __init__(self, weapon:str, cartridge:str, ammo:str, velocity:int, building:str, Marina:int, gravity:int):
+        self.weapon = weapon
+        self.cartridge = cartridge
+        self.ammo = ammo
+        self.velocity = velocity
+        self.building = building
+        self.Marina = Marina
+        self.gravity = gravity
+        
