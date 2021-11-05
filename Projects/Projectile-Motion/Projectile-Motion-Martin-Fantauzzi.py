d0b9461be26dc5acc11c5c9e1c6b528cfc698750
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2021-11-05 14:57:26 -0400

    projectile motion

--- Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
+++ Projects/Projectile-Motion/Projectile-Motion-Martin-Fantauzzi.py
@@ -1,22 +1,25 @@
 import math
 from ExperimentalData import ExperimentalData
 
-# # weapon = 'VSS' 
-# # cartridge = '9x39mm'
-# # ammo = 'SPP gs'
-# # velocity = 310
+from pathlib import Path
+import json
+import os
+# # experimentalData.weapon = 'VSS' 
+# # experimentalData.cartridge = '9x39mm'
+# # experimentalData.ammo = 'SPP gs'
+# # experimentalData.velocity = 310
 
-# # building = 'Marina Torch'
-# # Marina = 352
+# # experimentalData.building = 'Marina Torch'
+# # experimentalData.height = 352
 
-# # gravity = 9.81
+# # experimentalData.gravity = 9.81
 
 def ProjectileFunction(experimentalData:ExperimentalData):
-    time = math.sqrt((2 * Marina) / gravity)
+    time = math.sqrt((2 * experimentalData.height) / experimentalData.gravity)
     # distance = (experimentalData[velocity] * time)
-    distance = (velocity * time)
+    distance = (experimentalData.velocity * time)
 
-    print(f'I chose the {weapon} sniper rifle as my weapon. Its cartridge is a {cartridge} and it fires single {ammo} rounds at {velocity} meters per second. The building I chose was the {building} that reaches {Marina} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.')
+    print(f"I chose the {experimentalData.weapon} sniper rifle as my weapon. Its cartridge is a {experimentalData.cartridge} and it fires single {experimentalData.ammo} rounds at {experimentalData.velocity} meters per second. The building I chose was the {experimentalData.building} that reaches {experimentalData.height} meters. The bullet round took {time} seconds to reach the ground and travelled {distance} meters.")
 
 # Convert parameters (variables) into a JSON 
 
@@ -27,10 +30,29 @@ def ProjectileFunction(experimentalData:ExperimentalData):
 # 'ammo' : 'SPP gs',
 # 'velocity' : 310,
 # 'building' : 'Marina Torch',
-# 'Marina' : 352,
+# 'height' : 352,
 # 'gravity' : 9.81
 
 # }
 
 experimentalData = ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
-ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
+
+myDataSet = [
+    ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81),
+    ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Shanghai World Financial Center', 492, 9.81),
+    ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Princess Tower', 413.4, 9.81),
+    ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Empire State Building', 381, 9.81),
+    ExperimentalData('VSS','9x39mm', 'SPP gs', 310, 'Bank of China Tower', 367, 9.81)
+    ]
+
+for data in myDataSet:
+    print('\n-------------------------------------------------------\n')
+    ProjectileFunction(data)
+
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'Projectile.Motion.json')
+
+with open(myOutputFilePath, 'w') as outfile: 
+    json.dump([data.__dict__ for data in myDataset], outfile) 
+
+# ProjectileFunction('VSS','9x39mm', 'SPP gs', 310, 'Marina Torch', 352, 9.81)
