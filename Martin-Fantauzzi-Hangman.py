84a3a08624c2006513599b097c341c8e13dcccb2
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2022-01-28 15:09:02 -0400

    Update Martin-Fantauzzi-Hangman.py

--- Martin-Fantauzzi-Hangman.py
+++ Martin-Fantauzzi-Hangman.py
@@ -7,7 +7,7 @@ def get_word(word_list):
     return word.upper()
 
 def play(word):
-    word_completion = "_" * len(word) 
+    word_completion = "_ " * len(word) 
     guessed = False 
     guessed_letters = []
     guessed_words = [] 
@@ -27,8 +27,32 @@ def play(word):
                 tries -= 1
                 guessed_letters.append(guess)
             else:
-                print("Great!", guess, "Perfect!") 
+                print("Great!", guess, "is correct!") 
                 guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "-" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried this", guess, ":/")
+            elif guess != word:
+                print(guess,"Incorrect :(")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                print("Invalid input")
+            print(display_hangman(tries))
+            print(word_completion)
+            print("\n")
+
+    if guessed:
+        print("Success! You got it right!")
+    else:
+        print("You have been hanged :(. The word was " + word + " Try Again!")
 
 
 def display_hangman(tries):
@@ -95,4 +119,18 @@ def display_hangman(tries):
        |      
        |      
       ---
-      """]
+      """,
+    ] 
+    return stages[tries]
+
+def main():
+    word = get_word(word_list)
+    play(word)
+    while input("Again? (Y/N)").upper() == "Y":
+        word = get_word(word_list)
+        play(word)
+
+if __name__ == "__main__":
+    main()
+
+
