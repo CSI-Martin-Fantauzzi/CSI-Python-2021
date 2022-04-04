e5a1b8b34e0b1feb2466fe3ff618a1b31fa6760f
CSI-Martin-Fantauzzi <23-021@sanignacio.pr>
2022-04-04 14:47:18 -0400

    webscraping05

---
+++ Projects/Web-Scraping/Webscraping05-Martin-Fantauzzi.py
@@ -0,0 +1,20 @@
+import requests
+import bs4
+
+website = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7749&lon=-122.4194#.Yks0di-B1QI")
+forecast = bs4.BeautifulSoup(website.content, "html.parser")
+sevenDay = forecast.find(id="seven-day-forecast")
+forecast_items = sevenDay.find_all(class_="tombstone-container")
+tonight = forecast_items[1]
+#print(tonight.prettify())
+period = tonight.find(class_="period-name").get_text()
+print(period)
+desc = tonight.find(class_="short-desc").get_text()
+print(desc)
+temp = tonight.find(class_="temp temp-low").get_text()
+print(temp)
+
+
+
+
+
